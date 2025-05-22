410ac379a8e1af93a12b64723f7aa49bdd882008
sheqwxxx <sheqwx@sheqwxxxs-MacBook-Air.local>
2025-05-22 10:43:23 +0300

    Сделала один мейн

--- Go/lesson-4/functions_demo.go
+++ Go/lesson-4/functions_demo.go
@@ -2,16 +2,9 @@ package main
 
 import "fmt"
 
-/*
 func greet(name string) {
 	fmt.Printf("Привет, %s!\n", name)
 }
-
-func main() {
-	greet("Саша")
-	
-}
-
 
 func sumSlice(nums []int) int {
 	sum := 0
@@ -21,13 +14,6 @@ func sumSlice(nums []int) int {
 	return sum
 }
 
-func main() {
-	numbers := []int{15, 32, 3}
-	sum := sumSlice(numbers)
-	fmt.Println("Сумма:", sum)
-}
-
-
 func getColor(name string, colors map[string]string) string {
 	if color, exists := colors[name]; exists {
 		return color
@@ -35,32 +21,32 @@ func getColor(name string, colors map[string]string) string {
 	return "Цвет для этого имени не найден"
 }
 
-func main() {
-	colorMap := map[string]string{
-		"Саша": "черный",
-		"Алена": "красный",
-		"Маша": "белый",
-	}
-
-	fmt.Println(getColor("Саша", colorMap))
-	fmt.Println(getColor("Соня", colorMap))
-}
-*/
-
 func isEven(n int) bool {
 	return n%2 == 0
 }
 
 func main() {
-	num := 7
+	greet("Саша")
 
+	numbers := []int{15, 32, 3}
+	sum := sumSlice(numbers)
+	fmt.Println("Сумма:", sum)
+
+	colorMap := map[string]string{
+		"Саша": "черный",
+		"Алена": "красный",
+		"Маша": "белый",
+	}
+	fmt.Println(getColor("Саша", colorMap))
+	fmt.Println(getColor("Соня", colorMap))
+
+	num := 7
 	if isEven(num) {
-		fmt.Printf("%d — чётное число", num)
+		fmt.Printf("%d — чётное число\n", num)
 	} else {
-		fmt.Printf("%d — нечётное число \n", num)
+		fmt.Printf("%d — нечётное число\n", num)
 	}
 
 	fmt.Println("Число 4 чётное?", isEven(4))
 	fmt.Println("Число 7 чётное?", isEven(7))
 }
-
